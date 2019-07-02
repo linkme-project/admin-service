45804b054525ab5c3b8657a6f6d6ea3e873677eb
Keunhak Lim <limkeunhak@gmail.com>
2019-07-03 01:19:53 +0900

    Add GET, POST, DELETE methods on path /contents

--- src/v1/api/index.js
+++ src/v1/api/index.js
@@ -3,33 +3,86 @@ require('dotenv').config();
 const Router = require('koa-router');
 
 const contentController = require('../contents/content.controller');
+const RESULT_CODE = require('../constants').RESULT_CODE;
 
 const contentApi = new Router();
 
 contentApi.post('/contents', async ctx => {
-  const result = await contentController.create(ctx);
+  try {
+    const result = await contentController.create(ctx);
+    ctx.body = {
+      resultCode: result,
+      resultMessage: 
+        result == RESULT_CODE.FAIL ? 'insert content failure' :
+        result == RESULT_CODE.SUCCESS ? 'insert content success' : 
+        result == RESULT_CODE.INVALID_PARAMS ? 'invalid parameters' : null,
+      returnValue: null
+    };
+  } catch (ex) {
+    ctx.body = {
+      resultCode: RESULT_CODE.FAIL,
+      resultMessage: ex.message,
+      returnValue: null
+    };
+  }
 });
 
 contentApi.get('/contents', async ctx => {
-  const result = await contentController.search(ctx);
+  try {
+    const contents = await contentController.search(ctx);
+    ctx.body = {
+      resultCode: contents.length === 0 ? RESULT_CODE.DATA_EMPTY : RESULT_CODE.SUCCESS,
+      resultMessage: contents.length === 0 ? 'Data is not exist' : null,
+      returnValue: contents
+    };
+  } catch (ex) {
+    ctx.body = {
+      resultCode: ex.resultCode ? ex.resultCode : RESULT_CODE.FAIL,
+      resultMessage: ex.resultMessage ? ex.resultMessage : ex.message,
+      returnValue: null
+    };
+  }
 });
 
-contentApi.get('/contents/:sn', ctx => {
-  const result = contentController.searchSingle(ctx);
-  
-  ctx.body = result;
+contentApi.get('/contents/:sn', async ctx => {
+  try {
+    const content = await contentController.searchOne(ctx);
+    ctx.body = {
+      resultCode: content === null ? RESULT_CODE.DATA_EMPTY : RESULT_CODE.SUCCESS,
+      resultMessage: content === null? 'Data is not exist' : null,
+      returnValue: content
+    };
+  } catch (ex) {
+    ctx.body = {
+      resultCode: ex.resultCode ? ex.resultCode : RESULT_CODE.FAIL,
+      resultMessage: ex.resultMessage ? ex.resultMessage : ex.message,
+      returnValue: null
+    };
+  }
 });
 
-contentApi.put('/contents', ctx => {
-  const result = contentController.create(ctx);
-  
-  ctx.body = result;
+contentApi.put('/contents', ctx => {  
+  // TODO
 });
 
-contentApi.delete('/contents', ctx => {
-  const result = contentController.create(ctx);
-  
-  ctx.body = result;
+contentApi.delete('/contents', async ctx => {
+  try {
+    const result = await contentController.delete(ctx);
+    ctx.body = {
+      resultCode: result,
+      resultMessage: 
+        result == RESULT_CODE.FAIL ? 'mongodb delete failure' :
+        result == RESULT_CODE.SUCCESS ? 'successfully delete content' : 
+        result == RESULT_CODE.INVALID_PARAMS ? 'invalid parameters' : null,
+      returnValue: null
+    };
+  } catch (ex) {
+    ctx.body = {
+      resultCode: RESULT_CODE.FAIL,
+      resultMessage: ex.message,
+      returnValue: null
+    };
+  }
 });
 
 module.exports = contentApi;
--- src/v1/contents/content.controller.js
+++ src/v1/contents/content.controller.js
@@ -1,6 +1,8 @@
 const Content = require('../models/content');
+const validator = require('validator');
 const RESULT_CODE = require('../constants').RESULT_CODE;
 
+// return resultCode
 exports.create = async (ctx) => {
   const {
     title,
@@ -11,20 +13,23 @@ exports.create = async (ctx) => {
   } = ctx.request.body;
   
   // check parameters 
-  if (!title || !content || !userId || (type === undefined)) {
+  if (!title || !content || !userId || (type === undefined || type === null)) {
     return RESULT_CODE.INVALID_PARAMS;
   }
 
-  // get next sn 
-  let result = await Content.find({ type: type }).exec();
-  let nextSn = 0; 
-
-  if (result.length == 0) {
-    nextSn = 1;
-  } else {
-    nextSn = result.length;
+  if (!validator.isInt(type.toString())) {
+    return RESULT_CODE.INVALID_PARAMS;
   }
 
+  // get next sn
+  let result;
+  try {
+    result = await Content.find({ type: type }).exec();
+  } catch (ex) {
+    return RESULT_CODE.FAIL;
+  }
+
+  let nextSn = result.length + 1; // TODO: need to change
   let comments = [];
   let regDate = new Date();
 
@@ -41,25 +46,80 @@ exports.create = async (ctx) => {
   
   try {
     await newItem.save();
-  } catch (e) {
+  } catch (ex) {
     return RESULT_CODE.FAIL;
   }
 
   return RESULT_CODE.SUCCESS;  
 };
 
+// return contents array
 exports.search = async (ctx) => {
-  let result = await Content.find().exec();
+  const {
+    userId,
+    pageSize,
+    pageNo,
+    type
+  } = ctx.request.query;
+
+  // check parameters 
+  if (type === undefined || pageSize === undefined || pageNo === undefined) {
+    throw { resultCode: RESULT_CODE.INVALID_PARAMS, resultMessage: 'Invalid Parameters' };
+  }
+
+  if (!validator.isInt(type.toString())) {
+    throw { resultCode: RESULT_CODE.INVALID_PARAMS, resultMessage: 'Invalid Parameters' };
+  }
+
+  let result = await Content.find((userId ? { type, userId } : { type })).exec();
+  let refinedResult = JSON.parse(JSON.stringify( result ));
+  refinedResult.map(content => {
+    delete content._id;
+    delete content.__v;
+  });
+
+  return refinedResult;
 };
 
-exports.searchSingle = (ctx) => {
-  // TODO
+exports.searchOne = async (ctx) => {
+  const { type } = ctx.request.query;
+  const { sn } = ctx.params;
+
+  // check parameters 
+  if (type === undefined || sn === undefined) {
+    throw { resultCode: RESULT_CODE.INVALID_PARAMS, resultMessage: 'Invalid Parameters' };
+  }
+
+  if (!validator.isInt(type) || !validator.isInt(sn)) {
+    throw { resultCode: RESULT_CODE.INVALID_PARAMS, resultMessage: 'Invalid Parameters' };
+  }
+
+  let result = await Content.find({ type, sn }).exec();
+  if (result.length === 0) return null;
+
+  let refinedResult = JSON.parse(JSON.stringify( result[0] ));
+  delete refinedResult._id;
+  delete refinedResult.__v;
+  return refinedResult;
 };
 
 exports.update = (ctx) => {
   // TODO
 };
 
-exports.delete = (ctx) => {
-  // TODO
+exports.delete = async (ctx) => {
+  const { type, sn } = ctx.request.body;
+
+  // check parameters 
+  if (type === undefined || sn === undefined) {
+    throw { resultCode: RESULT_CODE.INVALID_PARAMS, resultMessage: 'Invalid Parameters' };
+  }
+
+  if (!validator.isInt(type.toString()) || !validator.isInt(sn.toString())) {
+    throw { resultCode: RESULT_CODE.INVALID_PARAMS, resultMessage: 'Invalid Parameters' };
+  }
+
+  let result = await Content.remove({ type, sn }).exec();
+  if (result.ok == 1) return RESULT_CODE.SUCCESS;
+  else return RESULT_CODE.FAIL;
 };
